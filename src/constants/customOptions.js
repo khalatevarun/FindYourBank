8acc7e11dbfbf78a16ab59db25bba1279a88ad9e
Varun Khalate <khalatevarun@gmail.com>
2021-12-25 01:25:21 +0530

    add: options for sorting based on categories and cities

---
+++ src/constants/customOptions.js
@@ -0,0 +1,13 @@
+export const category = [
+  { label: 'IFSC', value: 'ifsc' },
+  { label: 'BANK ID', value: 'bank_id' },
+  { label: 'DISTRICT', value: 'district' },
+  { label: 'BRANCH', value: 'branch' },
+];
+
+export const cities = [
+  { label: 'Mumbai', value: 'MUMBAI' },
+  { label: 'Pune', value: 'PUNE' },
+  { label: 'Hyderabad', value: 'HYDERABAD' },
+  { label: 'Agra', value: 'AGRA' },
+];
