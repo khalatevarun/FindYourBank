36bcbabd4a539def289f1a555b4b55cc52d54dcb
Varun Khalate <khalatevarun@gmail.com>
2021-12-25 16:43:33 +0530

    add: user's favorites banks in the table

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -44,7 +44,6 @@ const AppRouter = () => {
         element={
           <FavoritesScreen
             userData={userData}
-            addToFavorites={addToFavorites}
             removeFromFavorites={removeFromFavorites}
           />
         }
--- src/screens/FavoritesScreen/FavoritesScreen.js
+++ src/screens/FavoritesScreen/FavoritesScreen.js
@@ -1,5 +1,22 @@
-const FavoritesScreen = ({ userData, setUserData }) => {
-  return <div>Favorites Screen</div>;
-};
+import { Col, Row, Select, Space, Spin, Table } from 'antd';
+import { allbanksColumn } from '../../constants/table/allbanksColumn';
+import get from 'lodash/get';
+import { useState } from 'react';
 
+const FavoritesScreen = ({ userData, removeFromFavorites }) => {
+  const [loading, setLoading] = useState(false);
+
+  return (
+    <Spin spinning={loading}>
+      <div className="screen-title">Favorites</div>
+      <Table
+        columns={allbanksColumn({
+          removeFromFavorites,
+          userData,
+        })}
+        dataSource={get(userData, 'favorites')}
+      />
+    </Spin>
+  );
+};
 export default FavoritesScreen;
